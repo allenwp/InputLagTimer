9be2d2b3201a862fdbf42b62a88604c730f2f4ea
Allen Pestaluky <allenwp@live.ca>
2013-03-11 15:51:07 -0400

    effectively no changes: just moved stuff around

--- Window.cpp
+++ Window.cpp
@@ -5,35 +5,42 @@
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
-
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the window.
-//
-//  WM_PAINT	- Paint the window
-//  WM_DESTROY	- post a quit message and return
-//
-//
-LRESULT CALLBACK Window::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+HANDLE Window::CreateThread(HINSTANCE hInstance)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
+	return ::CreateThread(NULL, 0, Window::StartWindowThread, hInstance, 9, NULL);
+}
 
-	switch (message)
-	{
-	case WM_PAINT:
-    // TODO: The multitheaded GUI didn't handle this message type... Figure out why...
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code here...
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
+//
+//   FUNCTION: (formerly) InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+Window::Window(HINSTANCE hInstance)
+{
+  mHInstance = hInstance;
+
+  mHWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   ShowWindow(mHWnd, SW_SHOWDEFAULT);
+   UpdateWindow(mHWnd);
+}
+
+Window::~Window(void)
+{
+}
+
+DWORD WINAPI Window::StartWindowThread( __in  LPVOID lpParameter )
+{
+	Window window((HINSTANCE)lpParameter);
+
+	window.MessageLoop();
+
 	return 0;
 }
 
@@ -71,43 +78,35 @@ ATOM Window::MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-DWORD WINAPI Window::StartWindowThread( __in  LPVOID lpParameter )
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the window.
+//
+//  WM_PAINT	- Paint the window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK Window::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	Window window((HINSTANCE)lpParameter);
+	PAINTSTRUCT ps;
+	HDC hdc;
 
-	window.MessageLoop();
-
+	switch (message)
+	{
+	case WM_PAINT:
+    // TODO: The multitheaded GUI didn't handle this message type... Figure out why...
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
 	return 0;
-}
-
-HANDLE Window::CreateThread(HINSTANCE hInstance)
-{
-	return ::CreateThread(NULL, 0, Window::StartWindowThread, hInstance, 9, NULL);
-}
-
-//
-//   FUNCTION: (formerly) InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-Window::Window(HINSTANCE hInstance)
-{
-  mHInstance = hInstance;
-
-  mHWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-  CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
-   ShowWindow(mHWnd, SW_SHOWDEFAULT);
-   UpdateWindow(mHWnd);
-}
-
-Window::~Window(void)
-{
 }
 
 void Window::MessageLoop()
@@ -145,94 +144,3 @@ void Window::MessageLoop()
 		// TODO: usleep(100ULL);
 	}
 }
-
-
-
-
-
-
-// Old stuff from the window and entry point:
-
-//#define MAX_LOADSTRING 100
-//
-//// Global Variables:
-//HINSTANCE hInst;								// current instance
-//TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-//TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
-//
-//// Forward declarations of functions included in this code module:
-//ATOM				MyRegisterClass(HINSTANCE hInstance);
-//BOOL				InitInstance(HINSTANCE, int);
-//LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-//
-//int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-//                     _In_opt_ HINSTANCE hPrevInstance,
-//                     _In_ LPTSTR    lpCmdLine,
-//                     _In_ int       nCmdShow)
-//{
-//	UNREFERENCED_PARAMETER(hPrevInstance);
-//	UNREFERENCED_PARAMETER(lpCmdLine);
-//
-// 	// TODO: Place code here.
-//	MSG msg;
-//	HACCEL hAccelTable;
-//
-//	// Initialize global strings
-//	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-//	LoadString(hInstance, IDC_INPUTLAGTIMER, szWindowClass, MAX_LOADSTRING);
-//	MyRegisterClass(hInstance);
-//
-//	// Perform application initialization:
-//	if (!InitInstance (hInstance, nCmdShow))
-//	{
-//		return FALSE;
-//	}
-//
-//	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_INPUTLAGTIMER));
-//
-//	// Main message loop:
-//	while (GetMessage(&msg, NULL, 0, 0))
-//	{
-//		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-//		{
-//			TranslateMessage(&msg);
-//			DispatchMessage(&msg);
-//		}
-//	}
-//
-//	return (int) msg.wParam;
-//}
-//
-//
-//
-
-
-////
-////   FUNCTION: InitInstance(HINSTANCE, int)
-////
-////   PURPOSE: Saves instance handle and creates main window
-////
-////   COMMENTS:
-////
-////        In this function, we save the instance handle in a global variable and
-////        create and display the main program window.
-////
-//BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-//{
-//   HWND hWnd;
-//
-//   hInst = hInstance; // Store instance handle in our global variable
-//
-//   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-//      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-//
-//   if (!hWnd)
-//   {
-//      return FALSE;
-//   }
-//
-//   ShowWindow(hWnd, nCmdShow);
-//   UpdateWindow(hWnd);
-//
-//   return TRUE;
-//}
