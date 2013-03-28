f9753e8e861b34145abe5a1d10cd75ddcd7fdec5
Allen Pestaluky <allenwp@live.ca>
2013-03-28 18:21:23 -0400

    Starting point of window class. Currently has register class capabilities

--- InputLagTimer/Window.cpp
+++ InputLagTimer/Window.cpp
@@ -1,14 +1,61 @@
 #include "stdafx.h"
 #include "Window.h"
 
+TCHAR Window::windowClassName[] = _T("InputLagTimerWindowClassName");
+
+ATOM Window::registerWindow(HINSTANCE hInstance)
+{
+  WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= Window::WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= NULL;
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= windowClassName;
+	wcex.hIconSm		= NULL;
+
+	return RegisterClassEx(&wcex);
+}
+
+LRESULT CALLBACK Window::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
 
 Window::Window(void)
 {
+  windowName = new TCHAR[256]; /* 256 is max window name length */
+  wsprintf(windowName, _T("Input Lag Timer - Output %i/%i"), 1, 1);
 }
 
 
 Window::~Window(void)
 {
+  delete windowName;
 }
 
 bool operator<(const Window& lhs, const Window& rhs)
--- InputLagTimer/Window.h
+++ InputLagTimer/Window.h
@@ -3,10 +3,17 @@
 class Window
 {
 public:
+  static ATOM registerWindow(HINSTANCE hInstance);
+  static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+  static TCHAR windowClassName[];
+
   Window(void);
   virtual ~Window(void);
 
   unsigned int windowNumber;
+
+protected:
+  TCHAR* windowName;
 };
 
 // TODO: Verify that this is being called when accessing elements from the multimap
