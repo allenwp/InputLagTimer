d7fbee2de62778fe2526903a3d7975e063ee90a4
Allen Pestaluky <allenwp@live.ca>
2013-03-29 00:12:13 -0400

    Showing the window the same way that it was done in the original win32 code

--- InputLagTimer/Window.cpp
+++ InputLagTimer/Window.cpp
@@ -46,10 +46,17 @@ LRESULT CALLBACK Window::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM 
 	return 0;
 }
 
-Window::Window(void)
+Window::Window(HINSTANCE hInstance)
 {
   windowName = new TCHAR[256]; /* 256 is max window name length */
-  wsprintf(windowName, _T("Input Lag Timer - Output %i/%i"), 1, 1);
+  wsprintf(windowName, _T("Input Lag Timer - Output %i/%i"), 1, 1); // TODO
+
+   HWND hWnd = CreateWindow(windowClassName, windowName, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   ShowWindow(hWnd, SW_SHOWNORMAL);
+   UpdateWindow(hWnd);
+
 }
 
 
