f59d2a930c6d4dd468b046f84ac4769e0585934c
Allen Pestaluky <allenwp@live.ca>
2013-04-03 09:32:09 -0400

    Added timer logic

--- InputLagTimer/Model.cpp
+++ InputLagTimer/Model.cpp
@@ -2,8 +2,9 @@
 #include "Model.h"
 
 
-Model::Model(void)
+Model::Model(LARGE_INTEGER startingCount)
 {
+  mStartingCount = startingCount;
 }
 
 
@@ -13,5 +14,34 @@ Model::~Model(void)
 
 void Model::update()
 {
+  LARGE_INTEGER performanceFrequency;
+  QueryPerformanceFrequency(&performanceFrequency);
 
+  /* QUERY THE PERFORMANCE COUNTER!
+     The remaining of this function must be consistent between
+     models to ensure that the same latency to display occurs for
+     each output. */
+  LARGE_INTEGER currentCount;
+  QueryPerformanceCounter(&currentCount);
+
+  if(currentCount.QuadPart < mStartingCount.QuadPart)
+  {
+    reportError(ERROR_COUNTER_OVERFLOW, true);
+  }
+
+  LARGE_INTEGER countSinceStart;
+  countSinceStart.QuadPart = currentCount.QuadPart - mStartingCount.QuadPart;
+
+  double secondsSinceStart = ((double)countSinceStart.QuadPart) / performanceFrequency.QuadPart;
+
+  ULONGLONG fullTimerValue = secondsSinceStart * 100000.0 + 0.5; /* Add 0.5 to round */
+  ULONGLONG trimmedHigh = (fullTimerValue / 100000) * 100000;
+  unsigned int timerValue = fullTimerValue - trimmedHigh;
+
+  mLastCount = currentCount;
+}
+
+void Model::reportError(Model::ErrorType error, bool isPermanent)
+{
+  // TODO
 }
--- InputLagTimer/Model.h
+++ InputLagTimer/Model.h
@@ -2,9 +2,20 @@
 class Model
 {
 public:
-  Model(void);
+  enum ErrorType
+  {
+    ERROR_COUNTER_OVERFLOW /* Program needs restart! */
+  };
+
+  Model(LARGE_INTEGER startingCount);
   virtual ~Model(void);
 
   void update();
+
+protected:
+  void reportError(ErrorType error, bool isPermanent);
+
+  LARGE_INTEGER mStartingCount;
+  LARGE_INTEGER mLastCount;
 };
 
