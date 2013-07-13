cda63121d1211ed4c36cf66fb1d176e2e4c0e3fa
Allen Pestaluky <squirty33@hotmail.com>
2013-07-12 22:13:39 -0400

    Moved config file to top level of output directory to make it a little more obvious to users

--- InputLagTimer/Config.cpp
+++ InputLagTimer/Config.cpp
@@ -10,27 +10,27 @@ float Config::backgroundColour[4] = { 1.0, 1.0, 1.0, 1.0f };
 
 void Config::config()
 {
-  int lowestAccuracy = GetPrivateProfileInt(L"FAILSAFES", L"lowest_accuracy", 10, L".\\res\\config.ini");
+  int lowestAccuracy = GetPrivateProfileInt(L"FAILSAFES", L"lowest_accuracy", 10, L".\\config.ini");
   Config::lowestAccuracy = (double)lowestAccuracy / 10000.0;
-  int highestJitter = GetPrivateProfileInt(L"FAILSAFES", L"highest_jitter", 10, L".\\res\\config.ini");
+  int highestJitter = GetPrivateProfileInt(L"FAILSAFES", L"highest_jitter", 10, L".\\config.ini");
   Config::highestJitter = (double)highestJitter / 10000.0;
   
-  Config::numColumns = GetPrivateProfileInt(L"DISPLAY", L"num_columns", 2, L".\\res\\config.ini");
+  Config::numColumns = GetPrivateProfileInt(L"DISPLAY", L"num_columns", 2, L".\\config.ini");
 
-  int colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_r", 255, L".\\res\\config.ini");
+  int colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_r", 255, L".\\config.ini");
   float r = getColourComponent(colour);
-  colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_g", 255, L".\\res\\config.ini");
+  colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_g", 255, L".\\config.ini");
   float g = getColourComponent(colour);
-  colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_b", 255, L".\\res\\config.ini");
+  colour = GetPrivateProfileInt(L"DISPLAY", L"font_colour_b", 255, L".\\config.ini");
   float b = getColourComponent(colour);
   DirectX::XMVECTOR newColour = { r, g, b, 1.0f };
   fontColour = newColour;
 
-  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_r", 0, L".\\res\\config.ini");
+  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_r", 0, L".\\config.ini");
   getColourComponent(colour, &backgroundColour[0]);
-  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_g", 0, L".\\res\\config.ini");
+  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_g", 0, L".\\config.ini");
   getColourComponent(colour, &backgroundColour[1]);
-  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_b", 0, L".\\res\\config.ini");
+  colour = GetPrivateProfileInt(L"DISPLAY", L"background_colour_b", 0, L".\\config.ini");
   getColourComponent(colour, &backgroundColour[2]);
   backgroundColour[3] = 1.0f;
 }
